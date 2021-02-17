e587967a5898e6ece35d16a42464f4c5a05ba837
Joseph Pire <josephpire.dev@gmail.com>
2021-02-17 20:23:07 +0100

    feat(template): Improve contact page structure

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,86 +2,90 @@
   <Layout>
     <h1 class="aka-section-heading">Contact</h1>
 
-    <div class="flex flex-col lg:flex-row justify-between">
-      <aside>
-        <div class="flex flex-col justify-start items-end text-right">
-          <p class="text-3xl">Crepin Christopher</p>
-        </div>
+    <div class="flex flex-col lg:flex-row justify-between mt-16 mb-8">
+      <div class="lg:w-1/2 lg:mr-10">
+        <aside>
+          <div class="flex flex-col justify-start items-end text-right">
+            <p class="text-3xl">Crepin Christopher</p>
+          </div>
 
-        <div class="flex flex-col justify-start items-end my-8">
-          <p class="flex items-center">
-            {{ $page.contactData.edges[0].node.adress_street }}
-            <span class="inline-flex justify-end w-8 items-center"
-              ><MapPinIcon size="20" class="inline"
-            /></span>
-          </p>
-          <p>
-            {{ $page.contactData.edges[0].node.adress_city }}
-            <span class="inline-flex justify-end w-8 items-center"></span>
-          </p>
-        </div>
-        <div class="flex flex-col justify-start items-end my-8">
-          <a
-            :href="$page.contactData.edges[0].node.facebook_adress"
-            target="blank"
-            rel="noreferrer noopener"
-            class="flex items-center"
-          >
-            {{ $page.contactData.edges[0].node.facebook_title }}
-            <span class="inline-flex justify-end w-8 items-center"
-              ><FacebookIcon size="20" class="inline" /></span
-          ></a>
+          <div class="flex flex-col justify-start items-end my-8">
+            <p class="flex items-center">
+              {{ $page.contactData.edges[0].node.adress_street }}
+              <span class="inline-flex justify-end w-8 items-center"
+                ><MapPinIcon size="20" class="inline"
+              /></span>
+            </p>
+            <p>
+              {{ $page.contactData.edges[0].node.adress_city }}
+              <span class="inline-flex justify-end w-8 items-center"></span>
+            </p>
+          </div>
+          <div class="flex flex-col justify-start items-end my-8">
+            <a
+              :href="$page.contactData.edges[0].node.facebook_adress"
+              target="blank"
+              rel="noreferrer noopener"
+              class="flex items-center"
+            >
+              {{ $page.contactData.edges[0].node.facebook_title }}
+              <span class="inline-flex justify-end w-8 items-center"
+                ><FacebookIcon size="20" class="inline" /></span
+            ></a>
 
-          <p class="flex items-center my-2">
-            {{ $page.contactData.edges[0].node.email }}
-            <span class="inline-flex justify-end w-8 items-center"
-              ><MailIcon size="20" class="inline"
-            /></span>
-          </p>
-          <p class="flex items-center">
-            {{ $page.contactData.edges[0].node.phone }}
-            <span class="inline-flex justify-end w-8 items-center"
-              ><PhoneIcon size="20" class="inline"
-            /></span>
-          </p>
-        </div>
-        <div class="flex flex-col justify-start items-end">
-          <p class="flex items-center">
-            {{ $page.contactData.edges[0].node.vat }}
-            <span class="inline-flex justify-end w-8 items-center"
-              ><DollarSignIcon size="20" class="inline"
-            /></span>
-          </p>
-        </div>
-      </aside>
+            <p class="flex items-center my-2">
+              {{ $page.contactData.edges[0].node.email }}
+              <span class="inline-flex justify-end w-8 items-center"
+                ><MailIcon size="20" class="inline"
+              /></span>
+            </p>
+            <p class="flex items-center">
+              {{ $page.contactData.edges[0].node.phone }}
+              <span class="inline-flex justify-end w-8 items-center"
+                ><PhoneIcon size="20" class="inline"
+              /></span>
+            </p>
+          </div>
+          <div class="flex flex-col justify-start items-end">
+            <p class="flex items-center">
+              {{ $page.contactData.edges[0].node.vat }}
+              <span class="inline-flex justify-end w-8 items-center"
+                ><DollarSignIcon size="20" class="inline"
+              /></span>
+            </p>
+          </div>
+        </aside>
+      </div>
 
-      <form
-        action="https://formspree.io/f/mwkwjlra"
-        method="POST"
-        class="w-full lg:w-2/5 mt-12 lg:mt-0"
-      >
-        <label for="name">Nom :</label>
-        <input type="text" name="name" />
-
-        <label for="email">Email :</label>
-        <input type="email" name="_replyto" />
-
-        <label for="phone">Téléphone :</label>
-        <input type="phone" name="telephon" />
-
-        <label for="subject">Sujet :</label>
-        <input type="text" name="subject" />
-
-        <label for="Message">Message :</label>
-        <textarea name="message" rows="10" />
-
-        <button
-          class="py-1 px-4 bg-primary dark:bg-secondary text-black w-full mt-2 rounded"
-          type="submit"
+      <div class="lg:w-1/2 lg:ml-10">
+        <form
+          action="https://formspree.io/f/mwkwjlra"
+          method="POST"
+          class="w-full md:w-5/6 mt-12 lg:mt-0"
         >
-          Envoyer
-        </button>
-      </form>
+          <label for="name">Nom :</label>
+          <input type="text" name="name" />
+
+          <label for="email">Email :</label>
+          <input type="email" name="_replyto" />
+
+          <label for="phone">Téléphone :</label>
+          <input type="phone" name="telephon" />
+
+          <label for="subject">Sujet :</label>
+          <input type="text" name="subject" />
+
+          <label for="Message">Message :</label>
+          <textarea name="message" rows="10" />
+
+          <button
+            class="py-1 px-4 bg-primary dark:bg-secondary text-black w-full mt-2 rounded"
+            type="submit"
+          >
+            Envoyer
+          </button>
+        </form>
+      </div>
     </div>
   </Layout>
 </template>
