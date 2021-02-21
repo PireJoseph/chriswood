273b0464c4d2d2810d19b1c575621209993b3b9a
Joseph Pire <jpire.dev@gmail.com>
2021-02-21 19:53:33 +0100

    feat(template): improve app banner repsonsiveness

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,15 +1,17 @@
 <template>
   <section
-    class="aka-banner-wrapper relative w-full py-4 mb-8 md:grid md:gap-8 md:items-center md:mx-auto"
+    class="aka-banner-wrapper relative w-full py-4 my-12 flex items-center"
   >
-    <div class="text-left">
+    <div class="text-left lg:w-1/2">
       <p class="font-semibold text-xl leading-snug m-0 mb-2">
         {{ subheading }}
       </p>
       <h1 class="font-black text-5xl leading-none mb-2">{{ heading }}</h1>
       <slot />
     </div>
-    <div class="aka-banner-image hidden md:block">
+    <div
+      class="aka-banner-image hidden lg:block lg:w-1/2 max-w-xs flex justify-end items-end ml-auto"
+    >
       <AppImage
         v-if="mainImage"
         class="block w-full h-auto"
